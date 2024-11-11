4460f3d26e0b718faa1d758c3b6215c71079ed6d
Daniel Lounsbury <danielwlouns@gmail.com>
2024-11-10 23:34:30 -0800

    finish project 6

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # Project 6: Dijkstra's and ARP Sniffing # Daniel Lounsbury
 # Brian Hall
 # CS372
+# Collaborator(s): Brandon Nelson
 
-# Function(s) Documentation
+## Page used for reference 
+https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/ 
 
+## Function(s) Documentation
+
+### `ipv4_to_value(ipv4_addr)`
+Convert a dots-and-numbers IP address to a single 32-bit numeric value of integer type. Returns an integer type.
+
+### `value_to_ipv4(addr)`
+Convert a single 32-bit numeric value of integer type to a dots-and-numbers IP address. Returns a string type.
+
+### `get_subnet_mask_value(slash)`
+Given a subnet mask in slash notation, return the value of the mask as a single number of integer type. The input can contain an IP address optionally, but that part should be discarded. Returns an integer type.
+
+### `ips_same_subnet(ip1, ip2, slash)`
+Given two dots-and-numbers IP addresses and a subnet mask in slash notation, return true if the two IP addresses are on the same subnet. Returns a boolean. This function utilizes `get_subnet_mask_value()` and `ipv4_to_value()`.
+
+### `get_network(ip_value, netmask)`
+Return the network portion of an address value as integer type.
+
+### `find_router_for_ip(routers, ip)`
+Search a dictionary of routers (keyed by router IP) to find which router belongs to the same subnet as the given IP. Return `None` if no router is on the same subnet as the given IP. This function calls `ips_same_subnet()`.
+
+### `dijkstras_shortest_path(routers, src_ip, dest_ip)`
+This function takes a dictionary representing the network, a source IP, and a destination IP, and returns a list with all the routers along the shortest path.
--- dijkstra.py
+++ dijkstra.py
@@ -2,7 +2,43 @@ import sys
 import json
 import math  # If you want to use math.inf for infinity
 
+def ipv4_to_value(ipv4_addr):
 
+    octets = map(int, ipv4_addr.split('.'))
+    return sum(octet << (8 * (3 - i)) for i, octet in enumerate(octets))
+
+
+def value_to_ipv4(addr):
+
+    return '.'.join(str((addr >> (8 * (3 - i))) & 0xFF) for i in range(4))
+
+
+def get_subnet_mask_value(slash):
+
+    prefix_length = int(slash.strip('/'))
+    mask = (1 << 32) - (1 << (32 - prefix_length))
+    return mask
+
+
+def ips_same_subnet(ip1, ip2, slash):
+
+    ipval1 = ipv4_to_value(ip1)
+    ipval2 = ipv4_to_value(ip2)
+    netmask = get_subnet_mask_value(slash)
+    return (get_network(ipval1, netmask)) == (get_network(ipval2, netmask))
+
+
+def get_network(ip_value, netmask):
+
+    return ip_value & netmask
+
+
+def find_router_for_ip(routers, ip):
+
+    for router_ip, router_info in routers.items():
+        if ips_same_subnet(ip, router_ip, router_info["netmask"]):
+            return router_ip
+    return None
 
 def dijkstras_shortest_path(routers, src_ip, dest_ip):
     """
@@ -57,7 +93,41 @@ def dijkstras_shortest_path(routers, src_ip, dest_ip):
     function. Having it all built as a single wall of code is a recipe
     for madness.
     """
-        
+    src_router = find_router_for_ip(routers, src_ip)
+    dest_router = find_router_for_ip(routers, dest_ip)
+
+    if not src_router or not dest_router:
+        return[]
+    
+    if ips_same_subnet(src_ip, dest_ip, routers[src_router]["netmask"]):
+        return[]
+    
+    dist = {router_ip: math.inf for router_ip in routers}
+    parents = {router_ip: None for router_ip in routers}
+    dist[src_router] = 0
+    to_visit = set(routers.keys())
+
+    while to_visit:
+        current = min(to_visit, key = lambda x: dist[x])
+        to_visit.remove(current)
+
+        for neighbor, connection in routers[current]['connections'].items():
+            alt = dist[current] + connection['ad']
+
+            if alt < dist[neighbor]:
+                dist[neighbor] = alt
+                parents[neighbor] = current
+    path = []
+    current_node = dest_router
+    while current_node != src_router:
+        if current_node is None:
+            return []
+        path.append(current_node)
+        current_node = parents[current_node]
+    path.append(src_router)
+    path.reverse()
+
+    return path
 
 
 
